0ab60e93ff8ac23b7df30c4f39e46414edfe132e
Anveks <vekselmana@gmail.com>
2024-04-27 18:57:14 +0300

    crud users added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, InternalServerErrorException } from "@nestjs/common";
+import { Body, Controller, Get, InternalServerErrorException, Param, Post, Put } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { User } from "./users.model";
 
@@ -6,13 +6,68 @@ import { User } from "./users.model";
 export class UsersController {
   constructor (private readonly usersService: UsersService) {}
 
+  @Post()
+  async create(
+    @Body("name") name: string,
+    @Body("email") email: string,
+    @Body("password") password: string,
+    @Body("address") address: string
+  ): Promise<User> {
+    const userObj = {
+      name: name,
+      email: email,
+      password: password,
+      address: address
+    };
+    const newUser = await this.usersService.create(userObj);
+    console.log(newUser);
+    return newUser;
+  };
+
+  // get all
   @Get()
-  async getUsers(): Promise<User[]> {
+  async findAll(): Promise<User[]> {
     try {
       const result = await this.usersService.getAllUsers(); // getting all users
       return result;
     } catch(err: any) {
       throw new InternalServerErrorException(err.message); // rethrow error message if occurs
     }
+  };
+
+  // find one by id
+  @Get(":id")
+  async findOne(
+    @Param('id') id: string
+  ): Promise<User> {
+    const user = await this.usersService.findOneUser(id);
+    return user;
+  };
+
+  // find one by email
+  @Get("search/email")
+  async findByEmail(
+    @Body("email") email: string
+  ): Promise<User> {
+    const user = await this.usersService.findByEmail(email);
+    return user;
+  };
+
+  @Put()
+  async update(
+    @Body("name") name: string,
+    @Body("email") email: string,
+    @Body("password") password: string,
+    @Body("address") address: string
+  ): Promise<User> {
+    const updatedUserObj = {
+      name: name,
+      email: email,
+      password: password,
+      address: address
+    };
+    const result = await this.usersService.update(updatedUserObj);
+    return result;
   }
+
 };
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,26 @@
 import { Injectable, InternalServerErrorException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
+import mongoose, { Model } from "mongoose";
 import { User } from "./users.model";
+import { UserType } from "src/shared/enums/user_type.enum";
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel("User") private readonly userModel: Model<User>){} // injecting the Schema  
+  constructor(@InjectModel("User") private readonly userModel: Model<User>){} // injecting the Schema 
+  
+  // add one 
+  async create(userObj: { name: string, email: string, password: string, address: string }): Promise<User> {
+    const newUser = new this.userModel({ // creating a new user with the help of mongoose-powered userModel
+      _id: new mongoose.Types.ObjectId(),
+      name: userObj.name,
+      email: userObj.email,
+      password: userObj.password, // should a hash password function be added here? TODO:
+      address: userObj.address,
+      type: UserType.USER // regular user by default 
+    });
+    const result = await newUser.save(); // saving in the db
+    return result;
+  };
 
   // get all
   async getAllUsers(){
@@ -17,4 +32,22 @@ export class UsersService {
       if (users.length === 0) throw new InternalServerErrorException("Error while fetching the users."); // if no users found - throw a server err
     }
   };
+
+  // get one
+  async findOneUser(_id: string): Promise<User> {
+    const user = await this.userModel.findById(_id).exec();
+    return user;
+  }
+
+  // find one by email 
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.userModel.findOne({email: email}).exec();
+    return user;
+  }
+
+  // update
+  async update(updatedUser): Promise<User>{
+    return null;
+  }
+
 }
